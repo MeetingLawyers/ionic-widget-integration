a16e59a3aa6705ed2ef745c898040d73911f7f52
Manel Roca <manel.roca@meetinglawyers.com>
2021-09-09 10:43:35 +0200

    add widget event handler

--- MLIntegration/src/app/home/home.page.ts
+++ MLIntegration/src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, HostListener } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
+import { Device } from '@ionic-native/device/ngx';
 
 
 declare var meetinglawyers: any;
@@ -12,11 +13,15 @@ declare var meetinglawyers: any;
 })
 export class HomePage {
 
-  constructor(private androidPermissions: AndroidPermissions, public platform: Platform) { }
+  constructor(private androidPermissions: AndroidPermissions, 
+              public platform: Platform, 
+              private device: Device) { }
 
   async ngOnInit() {
     this.initWidget()
   }
+
+  // *** WIDGET ***
 
   initWidget() {
     meetinglawyers.initialize({
@@ -28,65 +33,122 @@ export class HomePage {
       isEmbeddedApp: true,
       jwt: '<AUTHENTICATION_JWT>',
     });
-
-    if (this.platform.is("android")) {
-      this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.CAMERA).then(
-        result => this.updateWidgetPermissions(result.hasPermission),
-        err => {}
-      );
-  
-      this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.RECORD_AUDIO).then(
-        result => this.updateWidgetPermissions(result.hasPermission),
-        err => {}
-      );
-    }
   }
 
+  // Widget event handler
   @HostListener('document:meetingLawyers', ['$event', '$event.detail.type'])
-  onDocumentKeyUp2(event: CustomEvent, type: string) {
-    console.log("HOST LISTENER TRES!!! ");
-    console.log(event)
-    console.log(type)
+  meetingLawyersEventHandler(event: CustomEvent, type: string) {
     switch (type) {
       case "videocall-action":
+        // Triggered when the widget needs camera permissions
         this.requestCameraPermissions();
+        break;
+      case "initialized":
+        // Triggered when the widget is loaded
+        this.checkPermissions();
+        break;
+      case "register-token":
+        // Used in JWT integrations
+        this.checkPermissions();
         break;
       default:
         break;
     }
   }
 
-  @HostListener('document:meetinglawyers-initialized', ['$event'])
-  onDocumentKeyUp3(event: CustomEvent) {
-    console.log("HOST LISTENER CUATRO!!! ");
-    console.log(event)
-  }
-
-  requestCameraPermissions() {
-    if (this.platform.is("android")) {
-      // Android permissions
-      this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.CAMERA).then(
-        result => this.updateWidgetPermissions(result.hasPermission),
-        err => this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.CAMERA)
-      );
-  
-      this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.RECORD_AUDIO).then(
-        result => this.updateWidgetPermissions(result.hasPermission),
-        err => this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.RECORD_AUDIO)
-      );
-      
-      this.androidPermissions.requestPermissions([this.androidPermissions.PERMISSION.CAMERA, this.androidPermissions.PERMISSION.RECORD_AUDIO, this.androidPermissions.PERMISSION.MODIFY_AUDIO_SETTINGS]);
-    } else if (this.platform.is("ios")) {
-      // iOS permissions
-      this.updateWidgetPermissions(true)
-    }
-  }
-
-  updateWidgetPermissions(allowed: boolean) {
+  updateWidgetPermissions(allowed: boolean, suported = true) {
+    console.log("updateWidgetPermissions : " + allowed + " - " + suported);
     if (allowed) {
       meetinglawyers.setWidgetPermissions({audioVideo : "allowed"})
     } else {
       meetinglawyers.setWidgetPermissions({audioVideo : "not-allowed"})
     }
+    if (!suported) {
+      meetinglawyers.setWidgetPermissions({audioVideo : "not-suported"})
+    }
   }
+
+  // *** PERMISSIONS ***
+
+  checkPermissions() {
+    if (this.platform.is("android")) {
+      this.checkAndroidPermissions()
+    } else if (this.platform.is("ios")) {
+      this.checkIOSPermissions()
+    } else {
+      this.updateWidgetPermissions(true);
+    }
+  }
+
+  requestCameraPermissions() {
+    if (this.platform.is("android")) {
+      this.requestAndroidPermissions()
+    } else if (this.platform.is("ios")) {
+      this.requestIOSPermissions()
+    } else {
+      this.updateWidgetPermissions(true);
+    }
+  }
+
+  // *** ANDROID PERMISSIONS ***
+
+  checkAndroidPermissions() {
+    if (!this.platform.is("android")) return;
+
+    this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.CAMERA).then(
+      result => this.updateWidgetPermissions(result.hasPermission),
+      err => {}
+    );
+
+    this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.RECORD_AUDIO).then(
+      result => this.updateWidgetPermissions(result.hasPermission),
+      err => {}
+    );
+  }
+
+  requestAndroidPermissions() {
+    if (!this.platform.is("android")) return;
+    
+    this.androidPermissions.requestPermissions([this.androidPermissions.PERMISSION.CAMERA, this.androidPermissions.PERMISSION.RECORD_AUDIO, this.androidPermissions.PERMISSION.MODIFY_AUDIO_SETTINGS]).then(
+      success => {
+        this.updateWidgetPermissions(success);
+      },
+      err => this.updateWidgetPermissions(false)
+    ); 
+  }
+
+  // *** IOS PERMISSIONS ***
+
+  checkIOSPermissions() {
+    if (!this.platform.is("ios")) return;
+
+    if (this.compareVersion(this.device.version, "14.5") >= 0) {
+      // iOS permissions
+      this.updateWidgetPermissions(true)
+    } else {
+      // Not suported
+      this.updateWidgetPermissions(false, false)
+    }
+  }
+
+  requestIOSPermissions() {
+    this.checkIOSPermissions()
+  }
+
+  // *** HELPER ***
+
+  compareVersion(v1, v2) {
+    if (typeof v1 !== 'string') return false;
+    if (typeof v2 !== 'string') return false;
+    v1 = v1.split('.');
+    v2 = v2.split('.');
+    const k = Math.min(v1.length, v2.length);
+    for (let i = 0; i < k; ++ i) {
+        v1[i] = parseInt(v1[i], 10);
+        v2[i] = parseInt(v2[i], 10);
+        if (v1[i] > v2[i]) return 1;
+        if (v1[i] < v2[i]) return -1;        
+    }
+    return v1.length == v2.length ? 0: (v1.length < v2.length ? -1 : 1);
+}
 }
